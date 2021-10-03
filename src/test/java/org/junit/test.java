8e4510b940e511b3de0ae2a02601dd6024a8bf17
lenin945 <leninmathiyarasi@gmail.com>
2021-10-03 16:05:42 +0530

    Pgr updated

--- src/test/java/org/junit/test.java
+++ src/test/java/org/junit/test.java
@@ -1,5 +1,8 @@
 package org.junit;
 
-public @interface test {
-
+public class test {
+public static void main(String[] args) {
+	System.out.println("sxdcfvgbh");
+	System.out.println("Updated");
+}
 }
